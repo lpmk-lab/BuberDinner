86d8572e57601de5a53c5b8346738592656f34ef
lapyaeminkhant.sbs <lapyaeminkhant.sbs@gmail.com>
2023-09-25 22:49:08 +0630

    Global error Handling

---
+++ BuberDinner.Api/Middleware/ErrorHandlingMiddleware.cs
@@ -0,0 +1,41 @@
+﻿
+using System.Net;
+using System.Text.Json;
+
+namespace BuberDinner.Api.Middleware
+{
+    public class ErrorHandlingMiddleware
+    {
+
+        private readonly RequestDelegate _next;
+        public ErrorHandlingMiddleware(RequestDelegate next)
+        {
+            _next = next;
+        }
+
+        public async Task Invoke(HttpContext context )
+        {
+            try
+            {
+              await _next(context);
+            }
+            catch (Exception ex)
+            {
+                await handlExceptionAsync(context, ex);
+            }
+        }
+
+        public static Task handlExceptionAsync(HttpContext context, Exception ex)
+        {
+
+            var code = HttpStatusCode.InternalServerError;
+            var result = JsonSerializer.Serialize(new{ error = "An Error Occur when processing your request" });
+            context.Response.ContentType= "application/json";
+            context.Response.StatusCode=(int)code;
+            return context.Response.WriteAsJsonAsync(result);
+        }
+
+
+
+    }
+}
--- BuberDinner.Api/Program.cs
+++ BuberDinner.Api/Program.cs
@@ -1,3 +1,4 @@
+using BuberDinner.Api.Middleware;
 using BuberDinner.Application;
 using BuberDinner.infrastructure;
 
@@ -10,7 +11,7 @@ builder.Services.AddControllers();
 
 
 var app = builder.Build();{
-    
+app.UseMiddleware<ErrorHandlingMiddleware>();
 app.UseHttpsRedirection();
 app.MapControllers();
 app.Run();
